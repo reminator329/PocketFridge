b3e393ecdd74e3242c939728aece6fa9b3fcc7cb
Amélie CHRETIEN <ameliechretien@Amelies-MacBook-Pro.local>
2026-04-23 01:33:10 +0200

    Fix Apple

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import Firebase  // ← doit être présent
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      FirebaseApp.configure()  // ← doit être présent
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
